f3aa5676d3533897bf047e94a0dc7f8b665fa365
ayokown <133900164+ayokown@users.noreply.github.com>
2025-02-25 08:03:48 -0500

    Update mcdonaldscrapeapiedition.py

--- mcdonaldscrapeapiedition.py
+++ mcdonaldscrapeapiedition.py
@@ -23,18 +23,35 @@ def get_item_info(id: int) -> any:
     category = info["item"]["default_category"]["category"]["name"]
     name = info["item"]["item_name"]
     nutrition = {
-        "calories" : int(info["item"]["nutrient_facts"]["nutrient"][0]["value"]),
-        "protien" : int(info["item"]["nutrient_facts"]["nutrient"][0]["value"]),
-        
+        "calories" : (float(info["item"]["nutrient_facts"]["nutrient"][0]["value"]), "Cal."), # Format is (value, unit_of_measurement)
+        "protien" : (float(info["item"]["nutrient_facts"]["nutrient"][1]["value"]), "g"),
+        "carbohydrates" : (float(info["item"]["nutrient_facts"]["nutrient"][2]["value"]), "g"),
+        "unsaturated_fat" : (float(info["item"]["nutrient_facts"]["nutrient"][3]["value"]) -  float(info["item"]["nutrient_facts"]["nutrient"][5]["value"]), "g"),
+        "saturated_fat" : (float(info["item"]["nutrient_facts"]["nutrient"][5]["value"]), "g"),
+        "trans_fat" : (float(info["item"]["nutrient_facts"]["nutrient"][6]["value"]), "g"),
+        "total_fat" : (float(info["item"]["nutrient_facts"]["nutrient"][3]["value"]), "g"),
+        "sodium" : (float(info["item"]["nutrient_facts"]["nutrient"][4]["value"]), "mg"),
+        "cholesterol" : (float(info["item"]["nutrient_facts"]["nutrient"][7]["value"]), "mg"),
+        "fiber" : (float(info["item"]["nutrient_facts"]["nutrient"][8]["value"]), "g"),
+        "added_sugars" : (float(info["item"]["nutrient_facts"]["nutrient"][10]["value"]), "g"),
+        "total_sugars" : (float(info["item"]["nutrient_facts"]["nutrient"][9]["value"]), "g"),
+        "vitamin_d" : (float(info["item"]["nutrient_facts"]["nutrient"][11]["value"]), "mcg"),
+        "calcium" : (float(info["item"]["nutrient_facts"]["nutrient"][12]["value"]), "mg"),
+        "iron" : (float(info["item"]["nutrient_facts"]["nutrient"][13]["value"]), "mg"),
+        "potassium" : (float(info["item"]["nutrient_facts"]["nutrient"][14]["value"]), "mg"),
+        "vitamin_b6" : (float(info["item"]["nutrient_facts"]["nutrient"][15]["value"]), "mg")
     }
+    for nutrient in info["item"]["nutrient_facts"]["nutrient"]:
+        # Incomplete
     return (category, name, nutrition)
 
-for item in all_items:
-    if item.find_parent(id="maincatcontent"): # Ignores items in the "Featured Favorites" category as they are already in the normal menu
-        continue
-    else:
-        total_items += 1
-        item_id = item["data-product-id"] # Get the id of an item by finding its HTML attributes
-        item_info = get_item_info(item_id)
-        print(item_info)
-        break
+def create_item_list(filetowrite: any) -> any:
+    for item in all_items:
+        if item.find_parent(id="maincatcontent"): # Ignores items in the "Featured Favorites" category as they are already in the normal menu
+            continue
+        else:
+            total_items += 1
+            item_id = item["data-product-id"] # Get the id of an item by finding its HTML attributes
+            item_info = get_item_info(item_id)
+            print(item_info)
+            break
